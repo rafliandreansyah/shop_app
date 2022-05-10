834f584c94c6e3a752fd6284271957ec79b1d778
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2022-05-10 19:43:04 +0700

    add fututer builder widget

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -19,33 +19,50 @@ class _OrderScreenState extends State<OrderScreen> {
   var _isLoading = false;
   @override
   void initState() {
-    setState(() {
-      _isLoading = true;
-    });
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then(
-          (_) => setState(() {
-            _isLoading = false;
-          }),
-        );
+    // setState(() {
+    //   _isLoading = true;
+    // });
+    // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then(
+    //       (_) => setState(() {
+    //         _isLoading = false;
+    //       }),
+    //     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
     return Scaffold(
       drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your Order'),
       ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.items.length,
-              itemBuilder: (ctx, i) => OrderItem(orderData.items[i]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.error != null) {
+              return const Center(
+                child: Text('an error accurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (c, orderData, child) {
+                  return ListView.builder(
+                    itemCount: orderData.items.length,
+                    itemBuilder: (ctx, i) => OrderItem(orderData.items[i]),
+                  );
+                },
+              );
+            }
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
